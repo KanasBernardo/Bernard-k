823b666146fb43b356bb75958d8a067d795fc952
KanasBernardo <98572675+KanasBernardo@users.noreply.github.com>
2022-07-19 22:27:01 -0600

    h

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,16 @@ ScrollReveal().reveal('.footer', { delay: 600 });
 ScrollReveal().reveal('.footer-links', { delay: 600 });
 
 ScrollReveal().reveal('.social', { delay: 600 });
+
+window.sr = ScrollReveal();
+
+sr.reveal('.showcase', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px',
+});
+sr.reveal('.container', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px',
+});
